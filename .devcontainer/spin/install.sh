5fd903076b8e35b9c32a7311d66899324edce304
itowlson <ivan.towlson@fermyon.com>
2022-12-02 13:20:06 +1300

    Codespace test

---
+++ .devcontainer/spin/install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# This and the feature JSON also stolen from Craig
+
+VERSION="v0.6.0"
+OSARCH="linux-amd64"
+
+wget https://github.com/fermyon/spin/releases/download/${VERSION}/spin-${VERSION}-${OSARCH}.tar.gz
+tar xfv spin-${VERSION}-${OSARCH}.tar.gz
+su
+mv ./spin /usr/local/bin/
